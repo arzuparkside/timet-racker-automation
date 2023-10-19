05a55d855c82685c64d13f3319c25a816e7b5062
Derick Arzu <arzu@parkside.at>
2023-10-19 09:47:20 +0200

    Added URL and cleared Hours field

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: '',
+    baseUrl: 'https://time-tracker.builtby.parkside.at/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/tracking.cy.ts
+++ cypress/e2e/tracking.cy.ts
@@ -5,23 +5,19 @@ describe('Tracking page', () => {
     cy.get('#password').type('');
     cy.get('#kc-login').click();
 
-    cy.reload();
-    
-    cy.wait(120000);
-
     const date = new Date().toISOString();
 
     cy.get('textarea[formcontrolname=description]').type(`Test Automation Time Entry - ${date}`);
 
     cy.get('[formcontrolname=projectId] [role=combobox]').click();
 
-    cy.get('[role=option]').contains('Time Tracker Testing').click();
+    cy.get('[role=option]').contains('Minion').click();
 
     cy.get('[formcontrolname=serviceId] [role=combobox]').click();
 
     cy.get('[role=option]').contains('Quality Assurance').click();
 
-    cy.get('[formcontrolname=minutesFormatted]').type('8:00');
+    cy.get('[formcontrolname=minutesFormatted]').clear().type('8:00');
 
     cy.get('button[type=submit].add-btn').click();
   })
